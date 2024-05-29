ceedffa50b4565222b2d710617e723170469ffba
AnnTarou <ya.project.828121@outlook.com>
2024-05-29 15:04:30 +0900

    HTML変換メソッドをstaticクラスへ変更 UserLessonの表示をHTML変換型に修正

---
+++ HelloQuery/Method/MarkdownConverter.cs
@@ -0,0 +1,17 @@
+﻿using HelloQuery.Models;
+using Markdig;
+
+namespace HelloQuery.Method
+{
+    static public class MarkdownConverter
+    {
+        public static void ConvertMarkdownToHtml(Lesson selectedLesson)
+        {
+            selectedLesson.Description = Markdown.ToHtml(selectedLesson.Description);
+            selectedLesson.Question = Markdown.ToHtml(selectedLesson.Question);
+            selectedLesson.Hint = Markdown.ToHtml(selectedLesson.Hint);
+            selectedLesson.Answer = Markdown.ToHtml(selectedLesson.Answer);
+            selectedLesson.Reference = Markdown.ToHtml(selectedLesson.Reference);
+        }
+    }
+}
