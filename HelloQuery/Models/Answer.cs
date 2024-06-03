49f095c2ccfa48c43fdb0c79ff4f0cb1469579da
qirmiz <qirmiz9641@gmail.com>
2024-06-03 13:36:33 +0900

    Answerクラスにコメント追加

--- HelloQuery/Models/Answer.cs
+++ HelloQuery/Models/Answer.cs
@@ -3,7 +3,11 @@
     public class Answer
     {
         public int AnswerId { get; set; }
+
+        // Lessonの外部キー
         public int LessonId { get; set; }
+
+        // 有効な解答
         public string ValidAnswers { get; set; }
 
         public virtual Lesson Lesson { get; set; }
